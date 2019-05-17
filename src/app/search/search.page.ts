bf0d20652b06ee9fa2ce770fe8adacdd8a0face4
Lydia2010 <lidiya.s@gmail.com>
2019-05-17 13:55:19 -0400

    the component result was added

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {ModalController} from '@ionic/angular';
+import {ResultsComponent} from './results/results.component';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
-export class SearchPage implements OnInit {
-  query = '';
+export class SearchPage {
+  query: string;
 
-  constructor() { }
+  constructor(public modalController: ModalController) { }
 
-  ngOnInit() {
-  }
-  onSearch() {
-    console.log (this.query);
+
+  async onSearch() {
+    const modal = await this.modalController.create({
+      component: ResultsComponent
+    });
+    return await modal.present();
   }
 
 }
